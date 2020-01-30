d717d7c0f44bdb0b3da47a270087e165411a6a83
0778430 <0778430@KELCE-LABN21-P.pittstate.edu>
2020-01-30 09:02:32 -0600

    end of Day one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,31 @@
  * @author 0778430
  */
 public class Main {
-    
+
+    public static void main(String[] args) {
+        System.out.println("class 1 \t programing");
+        System.out.println("class 2 \t data base");
+
+        String outputText = "This will not \n";
+
+        System.out.println("Hello Class");
+        System.out.println("Round 2");
+        System.out.print(outputText + "be put together " + 1000);
+        System.out.println("this will "
+                + "allways"
+                + "work"
+                + "becaues it is smart");
+        //all can
+        int inches = 12;
+        double temperatureInF = 15.5;
+        //all can not
+        byte myNumber;
+        myNumber = 2;
+
+        boolean itsWarmOut = false;
+
+        char firstLetterOfMyFirstName = 'C';
+        String firstLetterOfMyLastName = "F";
+    }
+
 }
